3cda2b36a1bb66d91e5ed10c80fb98c782507323
Eddie Del Rio <edwinjdelrio@gmail.com>
2019-04-04 17:08:44 -0700

    Added an empty server that's listening on a port provided by .env

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('Hello world');
+const Express = require("express");
+const app = new Express();
+const port = process.env.PORT || 3000;
+
+function onListen()
+{
+    console.log("Server active on port : ", port);
+}
+
+app.listen(port, onListen);
