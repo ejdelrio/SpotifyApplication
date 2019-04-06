a6b20529b04e068ee8f7650f563f66f6807c9940
Eddie Del Rio <edwinjdelrio@gmail.com>
2019-04-06 13:13:45 -0700

    Implemented commonw work itemn methods in base class for sessions and operations

--- Operations/Operation.js
+++ Operations/Operation.js
@@ -1,8 +1,14 @@
 ﻿"use strict";
 
+const CommonwWorkItems = require("../Util/CommonWorkItems");
+
 class Operation
 {
+    IsStringNullOrWhiteSpace(str) { return CommonwWorkItems.IsStringNullOrWhiteSpace(str); }
 
+    ValidateType(parameter, expectedType) { return CommonwWorkItems.ValidateType(parameter, expectedType); }
+
+    ValidateIsNotNull(parameter, parameterName = "") { return CommonwWorkItems.ValidateIsNotNull(parameter, parameterName); }
 }
 
 module.exports = Operation;
--- ThirdPartySessionManagement/Session.js
+++ ThirdPartySessionManagement/Session.js
@@ -1,11 +1,19 @@
 ﻿"use strict";
 
+const CommonWorkItems = require("../Util/CommonWorkItems");
+
 class Session
 {
     constructor(sessionName)
     {
         this.sessionName = sessionName;
     }
+
+    IsStringNullOrWhiteSpace(str) { return CommonwWorkItems.IsStringNullOrWhiteSpace(str); }
+
+    ValidateType(parameter, expectedType) { return CommonwWorkItems.ValidateType(parameter, expectedType); }
+
+    ValidateIsNotNull(parameter, parameterName = "") { return CommonwWorkItems.ValidateIsNotNull(parameter, parameterName); }
 }
 
 module.exports = Session;
