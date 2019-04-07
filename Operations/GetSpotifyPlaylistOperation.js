c61d531e3c522e1d5c490b4532db0df3bba4391c
Eddie Del Rio <edwinjdelrio@gmail.com>
2019-04-06 18:14:48 -0700

    Implemented an operation to get a playlist from spotify

---
+++ Operations/GetSpotifyPlaylistOperation.js
@@ -0,0 +1,42 @@
+﻿"use strict";
+
+const Operation = require("./Operation");
+const SessionManager = require("../ThirdPartySessionManagement/SessionManager");
+const operationEndpoint = "/api/playlist/:playlistId"
+
+//=================================================
+// Private variables
+
+var playlistId;
+var userId;
+
+// End private variables
+// ================================================
+
+// ================================================
+// Private methods
+
+function ValidateRequestParameters(request, operationInstance)
+{
+
+}
+
+// End private methods
+// ===============================================
+
+class GetSpotifyPlaylistOperation extends Operation
+{
+    constructor()
+    {
+        super();
+    }
+
+    get Endpoint() { return new String(operationEndpoint); }
+
+    Invoke(request, response, next)
+    {
+        console.log("Entering GetSpotifyPlaylistOperation");
+    }
+}
+
+module.exports = new GetSpotifyPlaylistOperation();
--- Operations/Operation.js
+++ Operations/Operation.js
@@ -9,6 +9,11 @@ class Operation
     ValidateType(parameter, expectedType) { return CommonwWorkItems.ValidateType(parameter, expectedType); }
 
     ValidateIsNotNull(parameter, parameterName = "") { return CommonwWorkItems.ValidateIsNotNull(parameter, parameterName); }
+
+    Invoke(request, response, next)
+    {
+        throw new Error("Base implementation of Invoke cannot be used. Please overwrite it.");
+    }
 }
 
 module.exports = Operation;
